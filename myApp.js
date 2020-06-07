6d95e9ab16a2bf98f635746c71a99c390fc5e5a3
Isale <isale.bryan@gmail.com>
2020-06-07 15:02:27 +0300

    Chain Search Query Helpers to Narrow Search Reslts

--- myApp.js
+++ myApp.js
@@ -338,7 +338,10 @@ var removeManyPeople = function(done) {
 var queryChain = function(done) {
   var foodToSearch = "burrito";
   
-  done(null/*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 'asc'}).limit(2).select('-age').exec((err, data) =>{
+    err? done(err): done(null, data)
+  })
+
 };
 
 /** **Well Done !!**
